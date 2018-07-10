fb4871e5666f2a58b9e1c18112bf4f8aca2e043e
JOHNykn <40849489+JOHNykn@users.noreply.github.com>
2018-07-10 21:25:13 +0800

    Add files via upload

---
+++ vector/vector/binSearch_a.h
@@ -0,0 +1,10 @@
+#pragma once
+template<typename T> static Rank binSearch(T* A, T const& e, Rank lo, Rank hi) {
+	while (lo < hi) {
+		Rank mi = (lo + hi) >> 1;
+		if (e < A[mi]) hi = mi;
+		else if (A[mi] < e) lo = mi + 1;
+		else return mi;
+	}
+	return -1;
+}
