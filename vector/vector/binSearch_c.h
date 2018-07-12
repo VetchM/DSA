ee559a243f0fad67475a6e0a0ce4b9d881be5c2e
JOHNykn <40849489+JOHNykn@users.noreply.github.com>
2018-07-12 21:27:55 +0800

    Add files via upload

--- vector/vector/binSearch_c.h
+++ vector/vector/binSearch_c.h
@@ -1,9 +1,8 @@
 #pragma once
 template<typename T> static Rank binSearch(T* A, T const& e, Rank lo, Rank hi) {
-	while (1<hi-lo)
-	{
+	while (lo < hi) {
 		Rank mi = (lo + hi) >> 1;
-		(e < A[mi]) ? hi = mi : lo = mi;
+		(e < A[mi]) ? hi = mi : lo = mi + 1;
 	}
-	return (e == A[lo]) ? lo : -1;
+	return --lo;
 }
