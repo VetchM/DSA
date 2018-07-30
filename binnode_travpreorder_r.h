55e2dc75209221ed0e7ed95c02b9de68da046636
JOHNykn <40849489+JOHNykn@users.noreply.github.com>
2018-07-30 20:35:31 +0800

    Delete binnode_travpreorder_r.h

--- binnode_travpreorder_r.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-template <typename T,typename VST>
-void travPre_R(BinNodePosi(T) x, VST& visit) {
-	if (!x) return;
-	visit(x->data);
-	travPre_R(x->lc, visit);
-	tarvPre_R(->rc, visist);
-}
